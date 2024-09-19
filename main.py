aa86fa577e9fdf49215a40695ca0d0fc90ac04e9
azm <azmatraza27@gmail.com>
2024-09-19 21:06:22 +0530

    added upload and ocr

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-from fastapi import FastAPI
-from fastapi.params import Body
-from pydantic import BaseModel
+# AZM
+# 18/09/2024
 
 
+from typing import Optional
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import os
+import pytesseract
+from PIL import Image
 app = FastAPI()   # fastapi instance
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
+
 
 @app.get("/")
-def root():
-    return {"message" : "hello from fastapi! -- added something"}
+def all_files():
+    # returns all the uploaded files
+    path = f"/home/azm/projects/ocr_chatbot/uploaded_files"
+    return {"files" : os.listdir(path)}
 
+@app.post("/upload")
+async def upload(uploaded_file: UploadFile):
 
-@app.get("/posts")
-def get_posts():
-    return {"data": "some dummy data"}
-
-@app.post("/createpost")
-def create_post(new_post: Post):
-    print (new_post.dict())
-    return {"data": new_post.content}
+    if uploaded_file.content_type not in ['application/pdf', 'image/png', 'image/jpeg']:
+        raise HTTPException(status_code=400, detail="Only pdf and image(jpeg/png) files are accepted!")
+    try:
+        file_path = f"/home/azm/projects/ocr_chatbot/uploaded_files/{uploaded_file.filename}"
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.file.read())
+            curr_img = pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra')
+            print (curr_img)
+            return {"message": "file saved successfully", "filename": uploaded_file.filename, "filetype" : uploaded_file.content_type, "extracted_text": curr_img}
+    except Exception as e:
+        return {"message": e.args}
+    
